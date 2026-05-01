09a6661b3a0a15f12cba1c5529b6737d53f3adb4
Banu Ibrahimli <ibr.banu13@gmail.com>
2026-05-01 09:47:25 +0400

    question4 submission Fix the Functional Interface

---
+++ question4/LambdaFixTask.java
@@ -0,0 +1,12 @@
+package question4;
+
+interface NumberChecker {
+    boolean check(int number);
+    //void printResult();
+}
+public class LambdaFixTask {
+    public static void main(String[] args) {
+        NumberChecker isEven = number -> number % 2 == 0;
+        System.out.println(isEven.check(10));
+    }
+}
