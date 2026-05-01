db06eb6c7347584a25df6d25470fd31e8e8083aa
Banu Ibrahimli <ibr.banu13@gmail.com>
2026-05-01 09:50:33 +0400

    question7 submission Fix the Thread Bug

---
+++ question7/ThreadBug.java
@@ -0,0 +1,12 @@
+package question7;
+
+public class ThreadBug {
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+        System.out.println("Running in another thread");
+        });
+        //t.run();
+        t.start();
+        System.out.println("Main finished");
+    }
+}
